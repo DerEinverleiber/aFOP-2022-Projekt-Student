8aa94f361dd16943c06e0141de0320dc9fcca37a
DerEinverleiber <Jannis.Lutz@t-online.de>
2022-03-18 14:08:40 +0100

    Pasta Variants und Pizza leichte Veränderungen

--- src/main/java/projekt/food/PastaImpl.java
+++ src/main/java/projekt/food/PastaImpl.java
@@ -92,4 +92,121 @@ public class PastaImpl extends AbstractSaucable implements Pasta {
             return unaryOperator;
         }
     }
+
+    static class Variant<F extends PastaImpl, C extends Config> implements Pasta.Variant<PastaImpl, Config> {
+        private final String name;
+        private final FoodType<PastaImpl, Config> foodType;
+        private final BigDecimal basePrice;
+        private final double baseWeight;
+        private final double baseThickness;
+        private final String baseSauce;
+
+        /**
+         * This constructor initializes its parameters to the class' attributes
+         * @param name the name
+         * @param foodType the foodType
+         * @param basePrice the basePrice
+         * @param baseWeight the baseWeight
+         * @param baseThickness the baseThickness
+         * @param baseSauce the baseSauce
+         */
+        public Variant(String name, FoodType<PastaImpl, Config> foodType, BigDecimal basePrice, double baseWeight, double baseThickness, String baseSauce) {
+            this.name = name;
+            this.foodType = foodType;
+            this.basePrice = basePrice;
+            this.baseWeight = baseWeight;
+            this.baseThickness = baseThickness;
+            this.baseSauce = baseSauce;
+        }
+
+        /**
+         * The name of this variant.
+         *
+         * <p>
+         * This may be something similar to {@code "Pizza Margherita"}.
+         * </p>
+         *
+         * @return The name of this variant
+         */
+        @Override
+        public String getName() {
+            return name;
+        }
+
+        /**
+         * The food type in which this variant is grouped.
+         *
+         * <p>
+         * For example, if this variant was named {@code "Pizza Margherita"}, the matching food type would be {@code "Pizza"}.
+         * </p>
+         *
+         * @return The food type of this variant
+         */
+        @Override
+        public FoodType<PastaImpl, Config> getFoodType() {
+            return foodType;
+        }
+
+        /**
+         * The base price of this variant.
+         *
+         * @return The base price of this variant
+         */
+        @Override
+        public BigDecimal getBasePrice() {
+            return basePrice;
+        }
+
+        /**
+         * The base weight of this variant.
+         *
+         * @return The weight price of this variant
+         */
+        @Override
+        public double getBaseWeight() {
+            return baseWeight;
+        }
+
+        /**
+         * Creates an empty {@link Config} for this variant.
+         *
+         * @return An empty {@link Config} for this variant
+         */
+        @Override
+        public Config createEmptyConfig() {
+            return new Config(bigDecimal -> bigDecimal.add(getBasePrice()), operand -> operand + getBaseWeight(), s -> getBaseSauce() + " " + s, operand -> operand + getBaseThickness() );
+        }
+
+        /**
+         * Creates a new instance of {@link Food} described by this variant, its base values and modifications defined by the
+         * provided list of {@link Extra Extras}.
+         *
+         * @param extras The list of {@link Extra Extras} to configure the resultant {@link Food}
+         * @return An instance of {@link Food} based on the values from this variant and configured by the provided extras
+         */
+        @Override
+        public PastaImpl create(List<? extends Extra<? super Config>> extras) {
+            return null;
+        }
+
+        /**
+         * This method returns the base thickness
+         *
+         * @return baseThickness as double
+         */
+        @Override
+        public double getBaseThickness() {
+            return baseThickness;
+        }
+
+        /**
+         * Returns the baseSauce
+         *
+         * @return baseSauce as String
+         */
+        @Override
+        public String getBaseSauce() {
+            return baseSauce;
+        }
+    }
 }
